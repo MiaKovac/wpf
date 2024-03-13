712e414d1b1675f6f12d931b4258b176623a651e
vlaja123 <vladimirborocki1@gmail.com>
2024-03-13 08:53:52 +0100

    Prvi upload

---
+++ mia/Predmet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mia
+{
+	internal class Predmet
+	{
+		public string subjectName;
+		public List<double> marks = new List<double>();
+
+		public Predmet(string subject, List<double> Marks)
+		{
+			subjectName = subject;
+			marks = Marks;
+		}
+	}
+}
